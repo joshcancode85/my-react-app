368cfeedeadc96c87e3702a6379df15336b4608d
Joshua Davies <Joshua.Davies@henryschein.com>
2023-05-16 14:15:03 +0100

    finish login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    function goodLogin(){
-        navigate("/login/success");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (username === "bob" && password === "bob")
+            navigate("/login/success");
+        else
+            navigate("/login/failed");
     }
-
-    function badLogin(){
-        navigate("/login/failed");
-    }
-
-    return ( 
-    <div>
-        <h1>Login</h1>
-        <button onClick={goodLogin}>Good Login</button>
-        <button onClick={badLogin}>Bad Login</button>
-    </div> 
+    
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Enter your username:
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </label>
+                <label>Enter your password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <button type="submit">Login</button>
+            </form>
+        </div>
     );
 }
- 
+
 export default Login;
